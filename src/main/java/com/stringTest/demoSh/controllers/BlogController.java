98b18841e138e76cebffb2e7c53521b951dbc44c
Student <123@12d.dd>
2021-12-04 14:24:56 +0800

    Редактировать , удалить

--- src/main/java/com/stringTest/demoSh/controllers/BlogController.java
+++ src/main/java/com/stringTest/demoSh/controllers/BlogController.java
@@ -5,10 +5,7 @@ import com.stringTest.demoSh.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -48,7 +45,7 @@ public class BlogController {
     public String blogGetById(Model model,
                               @PathVariable(value = "id") long id) {
         //проверка ест ли такая статья
-        if (!postRepository.existsById(id)){
+        if (!postRepository.existsById(id)) {
             return "redirect:/blog";
         }
         Optional<Post> post = postRepository.findById(id);
@@ -59,5 +56,56 @@ public class BlogController {
 
         return "blog-detail";
     }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEditById(Model model,
+                               @PathVariable(value = "id") long id) {
+        //проверка ест ли такая статья
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        //чтобы проще работать, переводим в лист
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+
+        return "blog-edit";
+    }
+
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogEditPost(Model model,
+                               @PathVariable(value = "id") long id,
+                               @RequestParam String title,
+                               @RequestParam String anons,
+                               @RequestParam String fullText) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFullText(fullText);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogRemovePost(Model model,
+                                 @PathVariable(value = "id") long id)
+    {
+        Post post = postRepository.findById(id).orElseThrow();
+
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
+
+
+//
+//    @GetMapping("/rest")
+//    public Iterable blogMainRest(Model model) {
+//        Iterable<Post> posts = postRepository.findAll();
+//        model.addAttribute("posts", posts);
+//        return posts;
+//    }
 
 }
